565f392d1bc0223ce248614db6be65476aae50c4
1030443893@qq.com <1030443893@qq.com>
2023-02-28 23:35:06 +0800

    提交脚手架代码

---
+++ api-passenger/src/test/java/org/demo/apipassenger/Test1.java
@@ -0,0 +1,13 @@
+package org.demo.apipassenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test1 {
+
+    @Test
+    public void test1(){
+        System.out.println("测试方法1");
+    }
+}
